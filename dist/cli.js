2c2f42ca0f06056745648ab44e48a2b5ea19a6a6
julioacontreras <julio.aranis.contreras@gmail.com>
2021-11-24 20:32:43 +0100

    first commit

---
+++ dist/cli.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cli = void 0;
+function cli(args) {
+    console.log(args);
+}
+exports.cli = cli;
+//# sourceMappingURL=cli.js.map
